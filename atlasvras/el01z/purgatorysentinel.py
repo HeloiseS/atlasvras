62793a88154b5727599c2ba6ef31f2a35cdf390c
Heloise Stevance <hfstevance@gmail.com>
2024-12-07 17:00:48 +0000

    added the NEW weekly report bot and the purgatory sentinel NONE OF THESE ARE TESTED - I need to do that somehow maybe jack cna help

---
+++ atlasvras/el01z/purgatorysentinel.py
@@ -0,0 +1,108 @@
+from atlasapiclient import client as atlasapiclient
+from atlasapiclient.utils import API_CONFIG_FILE
+from atlasvras.utils.misc import fetch_vra_dataframe
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+import os
+import pandas as pd
+from datetime import datetime
+import logging
+import sys
+
+LOG_PATH = '/home/stevance/software/logs/'
+# Get the last run date from the command line arguments. This will be handled by the
+# bash script that will call this python script.
+try:
+    last_run_date = sys.argv[1]
+except IndexError:
+    raise IndexError("CL argument not found. Call script with [INCLUDE THE GREP COMMAND I'LL USE IN THE BASH SCRIPT]")
+# last_run_date = '2024-09-13'
+
+TODAY = datetime.today().strftime('%Y-%m-%d')
+# Configure logging
+logging.basicConfig(level=logging.INFO,
+                    # Set the logging level to DEBUG (you can change it as needed)
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    # Define the format of log messages
+                    filename=f'{LOG_PATH}/purgatorysentinel.log',
+                    # Specify the log file
+                    filemode='a')  # Set the file mode to 'w' for writing (change as needed)
+
+# Write in the log that we are starting the script
+logging.info("Starting the script - last run date: " + last_run_date)
+
+
+#####################################################33333
+EYEBALL_THRESHOLD = 4
+URL_BASE = 'https://star.pst.qub.ac.uk/sne/atlas4/candidate/'
+
+### Get all objects in Eyeball List right now
+get_ids_from_eyeball = atlasapiclient.GetATLASIDsFromWebServerList(api_config_file= API_CONFIG_FILE,
+                                         list_name='eyeball',
+                                         get_response=True
+                                         )
+
+# Use oldest data in ToDo list to set the datethreshold
+todo_list = atlasapiclient.RequestVRAToDoList(api_config_file=API_CONFIG_FILE)
+todo_list.get_response()
+# if the response is empty list  we want to exit the script
+if not todo_list.response:
+    logging.info("ToDo List is empty - Finished")
+    sys.exit()
+
+DATETHRESHOLD= pd.DataFrame(todo_list.response).sort_values('timestamp').timestamp.iloc[0]
+logging.info("Fetched the TODO List dataframe")
+
+vra_df = fetch_vra_dataframe(datethreshold=DATETHRESHOLD)
+vra_df.set_index('transient_object_id', inplace=True)
+
+# Crop df so only objects in eyeball list are left
+vra_df=vra_df.loc[get_ids_from_eyeball.atlas_id_list_int]
+logging.info("Got the VRA dataframe")
+
+# add a column recording the first timestamp for each object
+vra_df_w1st_time = vra_df.join(vra_df.reset_index().drop_duplicates('transient_object_id',
+                                                                                                                    keep='first').set_index('transient_object_id'
+                                                                                                                                                            )['timestamp'],
+                                                    rsuffix='_first')
+
+# PANDAS SHENANIGANS TO GET THE PHASE IN DAYS
+# Explanation: We want to add a column that is the delta time between first and current timestamp
+# 1. We need to convert to Timestamps before we take the Timedelta
+# 2. You can't do it in bulk because Timestamp takes one value at a time so we use a list comprehension
+# 3. We iterate over i the iloc (literally just the row number)
+# 4. Since with Timedelta we'd have to do the list comprehension as well I'm doing both the conversions
+# of both timestamp and timestamp_first AND putting them in Timedelta and getting the .days in one line
+# So it looks terrifying but I'm just ripping off the bandaid
+phase_days = [pd.Timedelta(
+                          pd.Timestamp(TODAY, tz='UTC'
+                                      )-pd.Timestamp(vra_df_w1st_time.timestamp_first.iloc[i]
+                                                    )
+                                      ).days
+                          for i in range(vra_df_w1st_time.shape[0])
+                         ]
+
+vra_df_w1st_time['phase_in_days'] = phase_days
+
+# I'm doing  a bit longer than 15 in case time zone shenanigans i didn't think of
+stuck_in_purgatory = vra_df_w1st_time[vra_df_w1st_time.phase_in_days>16].index.unique().values
+# output to csv the stuck in purgatory ids with today's date
+stuck_in_purgatory_df = pd.DataFrame(stuck_in_purgatory)
+stuck_in_purgatory_df['timestamp']=TODAY
+stuck_in_purgatory_df.to_csv(f'{LOG_PATH}/stuck_in_purgatory.csv',
+                                header=False, mode='a', index=False)
+
+
+TEXT_REPORT = "*Stuck in Purgatory*\n"
+for atlas_id in stuck_in_purgatory:
+    TEXT_REPORT+=f"- <{URL_BASE}{atlas_id}|{atlas_id}>\n"
+
+logging.info("Sending the report to slack")
+
+client = WebClient(token=os.getenv('SLACK_TOKEN_el01z'))
+client.chat_postMessage(
+  channel="#vra-dev",
+  text=TEXT_REPORT,
+)#"C0842K2QZS8",
+
+logging.info("Finished")
---
+++ atlasvras/el01z/weeklyreport.py
@@ -0,0 +1,272 @@
+# Slack API
+import os
+from slack_sdk import WebClient
+from atlasvras.utils.prettify import vra_colors, label_to_color
+from slack_sdk.errors import SlackApiError
+# St3ph3n API
+from atlasapiclient import client as atlasapiclient
+from atlasvras.utils.misc import fetch_vra_dataframe
+# generic things
+import requests
+import logging
+import sys
+from datetime import datetime
+# Analysis things
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+LOG_PATH = '/home/stevance/software/logs/'
+
+####################################################################################
+######################### STARTING THE SCRIPT ######################################
+
+# Get the last run date from the command line arguments. This will be handled by the
+# bash script that will call this python script.
+try:
+    last_run_date = sys.argv[1]
+except IndexError:
+    raise IndexError("CL argument not found. Call script with [INCLUDE THE GREP COMMAND I'LL USE IN THE BASH SCRIPT]")
+# last_run_date = '2024-09-13'
+
+TODAY = datetime.today().strftime('%Y-%m-%d')
+# Configure logging
+logging.basicConfig(level=logging.INFO,
+                    # Set the logging level to DEBUG (you can change it as needed)
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    # Define the format of log messages
+                    filename=f'{LOG_PATH}/weeklyreport.log',
+                    # Specify the log file
+                    filemode='a')  # Set the file mode to 'w' for writing (change as needed)
+
+# Write in the log that we are starting the script
+logging.info("Starting the script - last run date: " + last_run_date)
+
+#### CONSTANTS TO PRETIFY THE PLOTS ####
+plt.style.use('vra')
+
+
+
+def determine_alert_type(vra_table_row):
+    if vra_table_row['rank'] == -1:
+        return "auto-garbage"
+    if vra_table_row.preal == 0.0 and vra_table_row.pgal != 1.0:
+        return 'garbage'
+    elif vra_table_row.preal == 0.0 and vra_table_row.pgal == 1.0:
+        return 'pm'
+    elif vra_table_row.preal == 1.0 and vra_table_row.pgal == 1.0:
+        return 'galactic'
+    elif vra_table_row.preal == 1.0 and vra_table_row.pgal != 1.0:
+        return "good"
+    elif vra_table_row.preal == 0.5:
+        return "possible"
+
+
+###########################################################
+####### MAIN #######
+############################################################
+vra_past_week = fetch_vra_dataframe(datethreshold=last_run_date).set_index('transient_object_id')
+logging.info("Fetched the VRA datarame")
+
+TEXT_REPORT = f"*VRA Weekly Report*\n"
+
+#### N_RBScore > 0.2
+N_2EYEBALL= vra_past_week[(vra_past_week['rank']>4)
+                            ].index.unique().shape[0]
+N_RBSCORE_GT0p2 = vra_past_week[~pd.isna(vra_past_week.apiusername)].index.unique().shape[0]
+
+
+#### PIE CHART
+labels_ALL = vra_past_week.apply(determine_alert_type, axis=1).rename('type')
+df = labels_ALL[~pd.isna(labels_ALL)].value_counts()
+df.plot(kind='pie', autopct="%.2f%%", textprops={'color': 'k'},
+        colors=[label_to_color[label] for label in df.index.values], legend=True)
+
+plt.title(f'From {last_run_date} to {TODAY}')
+plt.savefig(f'{LOG_PATH}/figures/{TODAY}.png', bbox_inches='tight')
+
+##### NEW EVENTS AND POTENTIAL MISSES
+in_tns = vra_past_week[vra_past_week['rank'] == 10.0].index
+vra_past_week['type'] = labels_ALL
+potential_misses = vra_past_week.loc[in_tns][vra_past_week.loc[in_tns].type == 'good'].index.unique()
+trace = vra_past_week[pd.isna(vra_past_week.username)].loc[potential_misses]
+
+
+N_FOUND_FIRST = len(set(vra_past_week[vra_past_week.type == 'good'].index.unique()) - set(potential_misses))
+N_FOUND_BY_OTHERS = len(potential_misses)
+
+TEXT_REPORT +=f"*TL;DR*\n"
+TEXT_REPORT +=f":eye: *{N_2EYEBALL}* | :first_place_medal: *{N_FOUND_FIRST}* | :second_place_medal: *{N_FOUND_BY_OTHERS}*\n\n"
+
+TEXT_REPORT +=f"*The Deets*\n"
+TEXT_REPORT +=f"- Week Starting: {last_run_date}\n"
+TEXT_REPORT += (f"- You eyeballed *{N_2EYEBALL}* ({np.round(N_2EYEBALL/N_RBSCORE_GT0p2*100,1)}%) "
+                f"alerts out of the *{N_RBSCORE_GT0p2}* with RB > 0.2\n")
+TEXT_REPORT += (f"- **TNS**: *{N_FOUND_FIRST}* Reported by us first "
+                f"and another *{N_FOUND_BY_OTHERS}* (Cross-matched to TNS) \n\n")
+
+### THINGS THAT GOT LOW VRA SCORE BUT WERE IN TNS
+misses_index = set(trace[(trace['rank'] < 4)].index) - set(trace[(trace['rank'] > 4) & (trace['rank'] < 10)].index)
+N_POTENTIAL_MISSES = len(misses_index)
+
+RANKS = trace[trace['rank'] < 4].loc[list(misses_index)]['rank']
+
+#pstar_web_url_base = 'https://star.pst.qub.ac.uk/sne/atlas4/candidate/'
+
+TEXT_REPORT += f":warning: {N_POTENTIAL_MISSES} *Potential Misses* (low VRA scores but X-match raised their ranks.\n"
+#for index in misses_index:
+#   TEXT_REPORT += f"   - <{pstar_web_url_base}{index}| {index}> got a rank of {RANKS.loc[index]}\n"
+potential_misses_df = pd.DataFrame(misses_index)
+potential_misses_df['timestamp']=TODAY
+potential_misses_df.to_csv(f'{LOG_PATH}/potential_misses.csv',
+                                header=False, mode='a', index=False)
+
+logging.info("Analysis and Figure complete")
+
+with open(f"{LOG_PATH}/report.csv", "a") as f:
+    f.write(f"{TODAY},{N_RBSCORE_GT0p2},{N_2EYEBALL},{N_FOUND_FIRST},{N_FOUND_BY_OTHERS},{N_POTENTIAL_MISSES}\n")
+
+logging.info("numbers saved to report.csv")
+###################################################
+
+client = WebClient(token=os.getenv('SLACK_TOKEN_el01z'))
+
+file_path = f'{LOG_PATH}/figures/{TODAY}.png'
+
+# Slack API URL
+url = 'https://slack.com/api/files.upload'
+
+file_response = client.files_upload_v2(
+    #channels=["C07HZGBKHQX"],
+    channels=["C0842K2QZS8"], # #vra-dev to test
+    initial_comment=f"Here are the label distributions for the week starting on {TODAY}",
+    file=file_path,
+)
+file_url = file_response["file"]["permalink"]
+
+client.chat_postMessage(
+    #channels="C07HZGBKHQX", # #vra-forum for real thing
+    channel="C0842K2QZS8", # #vra-dev to test
+    text=TEXT_REPORT,
+)
+
+##### NEED TO DO THE DEV REPORTING #####
+TEXT_REPORT_DEV = f"*Rank Comparison for the week starting: {last_run_date}*\n"
+N_2EYEBALL_ALT = vra_past_week[(vra_past_week['rank_alt1']>4)
+                                ].index.unique().shape[0]
+
+TEXT_REPORT_DEV+= (f":eye: Number comparisons:  "
+                   f" :baby:{N_2EYEBALL}  |  :older_adult:{N_2EYEBALL_ALT}")
+### Making the comparison df
+# Vra for all the objects that were dealth with by NEW model
+# excludes the rows corresponding to crossmatching, garbaging and human decisions
+vra_decisions_df = vra_past_week[(vra_past_week.apiusername == 'vra')]
+vra_df_new_ranks = vra_decisions_df[(vra_decisions_df['rank']!=-1)
+                                    & (vra_decisions_df.rank_alt1.isna())
+                                    & (vra_decisions_df['rank'] != 10)]
+
+# Vra for all the objects that were dealth with by OLD model
+vra_df_old_ranks = vra_decisions_df[(vra_decisions_df['rank']!=-1)
+                                    & (~vra_decisions_df.rank_alt1.isna())
+                                    & (vra_decisions_df['rank'] != 10)]
+
+# Making the comparison dataframe
+comparison_df = vra_df_new_ranks[['rank']].join(vra_df_old_ranks.rank_alt1
+                                                )
+
+comparison_df.columns = ['rank_NEW', 'rank_OLD']
+
+# isolating human decisions
+human_decisions_df = vra_past_week[~(vra_past_week.apiusername == 'vra')]
+comparison_df = comparison_df.join(human_decisions_df.type)
+
+label_to_color = {'garbage':vra_colors['red'],
+                 'good':vra_colors['blue'],
+                 'pm':vra_colors['orange'],
+                 'galactic':vra_colors['yellow'],
+                 'auto-garbage':'grey',
+                 'possible': vra_colors['white'],
+                 np.nan: 'white'
+                }
+
+
+f, ax = plt.subplots(1, figsize=(5,5))
+ax.scatter(comparison_df.rank_NEW, comparison_df.rank_OLD,
+           color = [label_to_color[label] for label in comparison_df.type.values],
+           s=5, zorder=1000, marker='*', alpha=0.9)
+ax.axline((1, 1), slope=1, color='grey', ls='--')
+# Create a rectangle patch
+rect1 = patches.Rectangle((0, 4), 4, 6, linewidth=1, edgecolor=vra_colors['red'], facecolor='none')
+ax.axvspan(0, 4, ymin=4/10, ymax=10/10, alpha=0.3, color=vra_colors['red'])
+
+rect2 = patches.Rectangle((4, 0), 6, 4, linewidth=1, edgecolor=vra_colors['yellow'], facecolor='none')
+ax.axvspan(4, 10, ymin=0, ymax=4/10, alpha=0.3, color=vra_colors['yellow'])
+
+
+ax.axvspan(4, 10, ymin=4/10, ymax=10, alpha=0.3, color=vra_colors['blue'])
+# Add the rectangle to the plot
+ax.add_patch(rect1)
+ax.add_patch(rect2)
+ax.set_ylabel('BMO Rank')
+ax.set_xlabel('Crabby Rank')
+ax.set_ylim(0,10)
+ax.set_xlim(0,10)
+
+ax.text(0.1, 9.5, 'Danger', color=vra_colors['red'], fontsize=15)
+ax.text(10.1, 4.3, 'Agree', color=vra_colors['blue'], fontsize=15, rotation=90 )
+ax.text(10.1, 0.3, 'New', color=vra_colors['yellow'], fontsize=15, rotation=90)
+ax.set_title(f'From {last_run_date} to {TODAY}')
+plt.savefig(f'{LOG_PATH}/figures/{TODAY}_RankComparison.png', bbox_inches='tight')
+
+### Isolating just the scores from the models
+vra_scores_df = vra_decisions_df[(vra_decisions_df['rank']!=10.0)&
+                                 (vra_decisions_df['rank']!=-1.0)
+                                ]
+
+N_w_rank = sum(~vra_scores_df['rank'].isna())
+N_w_rank_alt1 = sum(~vra_scores_df['rank_alt1'].isna())
+
+N_MISSED_RANKS = N_w_rank - N_w_rank_alt1
+
+TEXT_REPORT_DEV+= (f"\n:warning: Old model would have *{N_MISSED_RANKS}* missing rank values "
+                   f"(:baby: {N_w_rank} | :older_adult:{N_w_rank_alt1} )\n")
+
+### Looking at how many garbage avoided and how many goods "saved"
+real_ids = set(human_decisions_df[human_decisions_df.preal==1.0].index)
+bogus_ids = set(human_decisions_df[human_decisions_df.preal==0.0].index)
+_index_set = set(vra_scores_df.index)
+
+vra_scores_real = vra_scores_df.loc[list(real_ids.intersection(_index_set))]
+vra_scores_bogus = vra_scores_df.loc[list(bogus_ids.intersection(_index_set))]
+
+N_AVOIDED_GARBAGE =  vra_scores_bogus[vra_scores_bogus['rank_alt1']<4].shape[0]
+
+missed_OLDmodel_typecount = human_decisions_df.loc[vra_scores_real[vra_scores_real['rank_alt1']<4].index].type.value_counts()
+N_SAVED_GOOD = missed_OLDmodel_typecount['good']
+N_SAVED_GAL = missed_OLDmodel_typecount['galactic']
+
+TEXT_REPORT_DEV+= f"\n:recycle: *{N_AVOIDED_GARBAGE}* garbage avoided\n"
+TEXT_REPORT_DEV+= f":boom: *{N_SAVED_GOOD}* goods saved\n"
+TEXT_REPORT_DEV+= f":milky_way: *{N_SAVED_GAL}* galactic saved\n"
+
+with open(f"{LOG_PATH}/dev_report.csv", "a") as f:
+    f.write(f"{TODAY},{N_2EYEBALL},{N_2EYEBALL_ALT},{N_MISSED_RANKS},{N_AVOIDED_GARBAGE},{N_SAVED_GOOD},{N_SAVED_GAL}\n")
+
+
+file_path = f'{LOG_PATH}/figures/{TODAY}_RankComparison.png'
+file_response = client.files_upload_v2(
+    channels=["C0842K2QZS8"],
+    initial_comment=f"Rank Comparisons for the past week",
+    file=file_path,
+)
+
+file_url = file_response["file"]["permalink"]
+
+client.chat_postMessage(
+    channel="C0842K2QZS8", # #vra-dev
+    text=TEXT_REPORT_DEV,
+)
+
+#########################################
+logging.info("Finished")
