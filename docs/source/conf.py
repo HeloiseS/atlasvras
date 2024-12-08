48127a0e312a052a186e71733e2059f93c74d41c
Heloise Stevance <hfstevance@gmail.com>
2024-12-08 17:45:41 +0000

    fixed?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-project = 'ATLAS API Client'
+project = 'ATLAS VRAs'
 copyright = '2024, Heloise Stevance'
 author = 'Heloise Stevance'
 
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 
 #import sphinx_rtd_theme
 
-#html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 
 #html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
