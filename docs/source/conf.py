45f746f625c53e9853a05d0a9bbe22b8e152adda
Heloise Stevance <hfstevance@gmail.com>
2024-12-08 17:12:33 +0000

    try again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,14 +5,19 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+project = 'ATLAS API Client'
+copyright = '2024, Heloise Stevance'
+author = 'Heloise Stevance'
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
 
-project = 'atlasvras'
-copyright = '2024, H.F.Stevance'
-author = 'H.F.Stevance'
+import sphinx_rtd_theme
 
+html_theme = 'sphinx_rtd_theme'
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 
 
@@ -27,11 +32,11 @@ extensions = [
 # The full version, including alpha/beta/rc tags
 release = __version__
 
-templates_path = ['_templates']
-exclude_patterns = []
+#templates_path = ['_templates']
+#exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+#html_theme = 'sphinx_rtd_theme'
+#html_static_path = ['_static']
