1de27133d897e2a1a99f3689793ea3b9738ab977
Heloise Stevance <hfstevance@gmail.com>
2025-01-30 16:18:20 +0000

    slackbot going to correct channel now

--- atlasvras/st3ph3n/slackbot.py
+++ atlasvras/st3ph3n/slackbot.py
@@ -106,7 +106,7 @@ client = WebClient(token=SLACK_TOKEN)
 
 try:
     response = client.chat_postMessage(
-        channel="#vra-dev",
+        channel="#vra",
         text=bot_message
     )
 except SlackApiError as e:
