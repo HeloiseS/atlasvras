c2d7016915ff649f0843d3c11c919adc0f6a7b63
Ken Smith <k.w.smith@qub.ac.uk>
2025-02-03 17:04:31 +0000

    Added modifications for weeklyreport.py to run in cron on QUB server. Likewise added vra.mplstyle and had to set backend as 'Agg' in the python code.

--- atlasvras/el01z/weeklyreport.py
+++ atlasvras/el01z/weeklyreport.py
@@ -40,9 +40,8 @@ import matplotlib.patches as patches
 import yaml
 import pkg_resources
 
-
-#### CONSTANTS TO PRETTIFY THE PLOTS ####
-plt.style.use('vra')
+import matplotlib
+matplotlib.use('Agg')
 
 #########################################
 # LOAD PATHS AND TOKENS FROM THE CONFIG FILE
@@ -67,10 +66,13 @@ with open(BOT_CONFIG_FILE, 'r') as stream:
 #####################################################################
 try:
     last_run_date = sys.argv[1]
+    stylefile = sys.argv[2]
 except IndexError:
     raise IndexError("CL argument not found. Call script with [INCLUDE THE GREP COMMAND I'LL USE IN THE BASH SCRIPT]")
 
 
+#### CONSTANTS TO PRETTIFY THE PLOTS ####
+plt.style.use(stylefile)
 TODAY = datetime.today().strftime('%Y-%m-%d')
 
 #################
@@ -128,7 +130,8 @@ TEXT_REPORT = f"*VRA Weekly Report*\n"
 
 #### N_RBScore > 0.2
 # TODO: this logic needs to change to reflect the new strat - easier if we have a column for galactic flag
-N_2EYEBALL= vra_past_week[(vra_past_week['rank']>4)
+N_2EYEBALL= vra_past_week[(vra_past_week['rank']>EYEBALL_THRESHOLD)
+                          | np.isclose(vra_past_week['is_gal_cand'],1.0)
                             ].index.unique().shape[0]
 N_RBSCORE_GT0p2 = vra_past_week[~pd.isna(vra_past_week.apiusername)].index.unique().shape[0]
 
@@ -164,10 +167,10 @@ TEXT_REPORT += (f"- *TNS*: *{N_FOUND_FIRST}* Reported by us first "
                 f"and another *{N_FOUND_BY_OTHERS}* (Cross-matched to TNS) \n\n")
 
 ### THINGS THAT GOT LOW VRA SCORE BUT WERE IN TNS
-misses_index = set(trace[(trace['rank'] < 4)].index) - set(trace[(trace['rank'] > 4) & (trace['rank'] < 10)].index)
+misses_index = set(trace[(trace['rank'] < EYEBALL_THRESHOLD)].index) - set(trace[(trace['rank'] > EYEBALL_THRESHOLD) & (trace['rank'] < 10)].index)
 N_POTENTIAL_MISSES = len(misses_index)
 
-RANKS = trace[trace['rank'] < 4].loc[list(misses_index)]['rank']
+RANKS = trace[trace['rank'] < EYEBALL_THRESHOLD].loc[list(misses_index)]['rank']
 
 TEXT_REPORT += f":warning: {N_POTENTIAL_MISSES} *Potential Misses* (low VRA scores but X-match raised their ranks.\n"
 #for index in misses_index:
@@ -196,8 +199,8 @@ client = WebClient(token=SLACK_TOKEN)
 file_path = f'{LOG_PATH}/figures/{TODAY}.png'
 
 file_response = client.files_upload_v2(
-    channel="C07HZGBKHQX",
-    #channels=["C0842K2QZS8"], # #vra-dev to test
+    channel="C07HZGBKHQX", #vra-forum
+    #channel="C0842K2QZS8", # #vra-dev to test
     initial_comment=f"Here are the label distributions for the week starting on {TODAY}",
     file=file_path,
 )
@@ -209,159 +212,6 @@ client.chat_postMessage(
     text=TEXT_REPORT,
 )
 
-
-###################################################
-#######################################
-#               VRA WEEKLY DEV REPORTING
-#######################################
-###################################################
-
-TEXT_REPORT_DEV = f"*Rank Comparison for the week starting: {last_run_date}*\n"
-N_2EYEBALL_ALT = vra_past_week[(vra_past_week['rank_alt1']>4)
-                                ].index.unique().shape[0]
-
-TEXT_REPORT_DEV+= (f":eye: Number comparisons:  "
-                   f" :baby:{N_2EYEBALL}  |  :older_adult:{N_2EYEBALL_ALT}")
-
-### Making the comparison df
-# Vra for all the objects that were dealth with by NEW model
-# excludes the rows corresponding to crossmatching, garbaging and human decisions
-vra_decisions_df = vra_past_week[(vra_past_week.apiusername == 'vra')]
-vra_df_new_ranks = vra_decisions_df[(vra_decisions_df['rank']!=-1)
-                                    & (vra_decisions_df.rank_alt1.isna())
-                                    & (vra_decisions_df['rank'] != 10)]
-
-# Vra for all the objects that were dealth with by OLD model
-vra_df_old_ranks = vra_decisions_df[(vra_decisions_df['rank']!=-1)
-                                    & (~vra_decisions_df.rank_alt1.isna())
-                                    & (vra_decisions_df['rank'] != 10)]
-
-# TODO: Not that this join is a problem when the index is not unique
-# not a bgi deal for the quick weekly report but worth keeping i mind.
-
-# Making the comparison dataframe
-comparison_df = vra_df_new_ranks[['rank']].join(vra_df_old_ranks.rank_alt1
-                                                )
-
-comparison_df.columns = ['rank_NEW', 'rank_OLD']
-
-# isolating human decisions
-human_decisions_df = vra_past_week[~(vra_past_week.apiusername == 'vra')]
-comparison_df = comparison_df.join(human_decisions_df.type)
-
-label_to_color = {'garbage':vra_colors['red'],
-                 'good':vra_colors['blue'],
-                 'pm':vra_colors['orange'],
-                 'galactic':vra_colors['yellow'],
-                 'auto-garbage':'grey',
-                 'possible': vra_colors['white'],
-                 None: 'white',
-                 np.nan: 'white'
-                }
-
-# PLOT THE COMPARISON
-f, ax = plt.subplots(1, figsize=(5,5))
-
-# Scatter of the ranks colour coded by the type of labelled already
-ax.scatter(comparison_df.rank_NEW, comparison_df.rank_OLD,
-           color = [label_to_color[label] for label in comparison_df.type.values],
-           s=5,
-           zorder=1000,
-           marker='*',
-           alpha=0.9)
-# just the line where old and new ranks are equal
-ax.axline((1, 1), slope=1, color='grey', ls='--')
-
-# Highlight the danger zone
-# (where new scores <4 and old scores >4)
-rect1 = patches.Rectangle((0, 4), 4, 6,
-                          linewidth=1,
-                          edgecolor=vra_colors['red'],
-                          facecolor='none')
-ax.add_patch(rect1)
-ax.axvspan(0, 4, ymin=4/10, ymax=10/10, alpha=0.3,
-           color=vra_colors['red'])
-ax.text(0.1, 9.5, 'Danger',
-        color=vra_colors['red'], fontsize=15)
-
-# Highlight the New stuff
-rect2 = patches.Rectangle((4, 0), 6, 4,
-                          linewidth=1, edgecolor=vra_colors['yellow'], facecolor='none')
-ax.add_patch(rect2)
-ax.axvspan(4, 10, ymin=0, ymax=4/10, alpha=0.3, color=vra_colors['yellow'])
-ax.text(10.1, 0.3, 'New', color=vra_colors['yellow'], fontsize=15, rotation=90)
-
-# Highlight the Agree zone
-ax.axvspan(4, 10, ymin=4/10, ymax=10, alpha=0.3, color=vra_colors['blue'])
-ax.text(10.1, 4.3, 'Agree', color=vra_colors['blue'], fontsize=15, rotation=90 )
-
-# Labels and limites
-ax.set_ylabel('BMO Rank')
-ax.set_xlabel('Crabby Rank')
-ax.set_ylim(0,10)
-ax.set_xlim(0,10)
-ax.set_title(f'From {last_run_date} to {TODAY}')
-
-# SAVE THE PLOT
-plt.savefig(f'{LOG_PATH}/figures/{TODAY}_RankComparison.png',
-            bbox_inches='tight')
-
-# MAKING THE NUMBERS WE WANT TO REPORT AND SAVE TO CSV
-
-### Isolating just the scores from the models
-vra_scores_df = vra_decisions_df[(vra_decisions_df['rank']!=10.0)&
-                                 (vra_decisions_df['rank']!=-1.0)
-                                ]
-
-N_w_rank = sum(~vra_scores_df['rank'].isna())
-N_w_rank_alt1 = sum(~vra_scores_df['rank_alt1'].isna())
-N_MISSED_RANKS = N_w_rank - N_w_rank_alt1
-
-# START WRITING THE SLACK MESSAGE
-TEXT_REPORT_DEV+= (f"\n:warning: Old model would have *{N_MISSED_RANKS}* missing rank values "
-                   f"(:baby: {N_w_rank} | :older_adult:{N_w_rank_alt1} )\n")
-
-### Looking at how many garbage avoided and how many goods "saved"
-real_ids = set(human_decisions_df[human_decisions_df.preal==1.0].index)
-bogus_ids = set(human_decisions_df[human_decisions_df.preal==0.0].index)
-_index_set = set(vra_scores_df.index)
-
-vra_scores_real = vra_scores_df.loc[list(real_ids.intersection(_index_set))]
-vra_scores_bogus = vra_scores_df.loc[list(bogus_ids.intersection(_index_set))]
-N_AVOIDED_GARBAGE =  vra_scores_bogus[vra_scores_bogus['rank_alt1']<4].shape[0]
-
-# How many goods and galactic were saved
-missed_OLDmodel_typecount = human_decisions_df.loc[vra_scores_real[vra_scores_real['rank_alt1']<4].index].type.value_counts()
-N_SAVED_GOOD = missed_OLDmodel_typecount['good']
-N_SAVED_GAL = missed_OLDmodel_typecount['galactic']
-
-# Finish writing the slack message
-TEXT_REPORT_DEV+= f"\n:recycle: *{N_AVOIDED_GARBAGE}* garbage avoided\n"
-TEXT_REPORT_DEV+= f":boom: *{N_SAVED_GOOD}* goods saved\n"
-TEXT_REPORT_DEV+= f":milky_way: *{N_SAVED_GAL}* galactic saved\n"
-
-##############
-# SAVE TO FILE
-##############
-with open(f"{LOG_PATH}/dev_report.csv", "a") as f:
-    f.write(f"{TODAY},{N_2EYEBALL},{N_2EYEBALL_ALT},{N_MISSED_RANKS},{N_AVOIDED_GARBAGE},{N_SAVED_GOOD},{N_SAVED_GAL}\n")
-
-#########################
-# SEND THE REPORT TO SLACK
-#########################
-file_path = f'{LOG_PATH}/figures/{TODAY}_RankComparison.png'
-file_response = client.files_upload_v2(
-    channels=["C0842K2QZS8"],
-    initial_comment=f"Rank Comparisons for the past week",
-    file=file_path,
-)
-
-file_url = file_response["file"]["permalink"]
-
-client.chat_postMessage(
-    channel="C0842K2QZS8", # #vra-dev
-    text=TEXT_REPORT_DEV,
-)
 
 #########################################
 logging.info("Finished")
