8f5692ec8703eefd1e34f40d55317b0dd7ba0187
Heloise Stevance <hfstevance@gmail.com>
2025-02-04 10:42:37 +0000

    purgatory sentinel now also looks at gal cand list

--- atlasvras/el01z/bash/weeklyreport.sh
+++ atlasvras/el01z/bash/weeklyreport.sh
@@ -7,4 +7,4 @@ export CODEBASE="/home/stevance/software/atlasvras/atlasvras/el01z"
 export LOGBASE="/home/stevance/software/logs/"
 
 export GREP=`/usr/bin/grep "Finished" $LOGBASE/weeklyreport.log | /usr/bin/tail -n 1 | /usr/bin/cut -d',' -f1 | /usr/bin/sed 's/ /T/'`
-$PYTHON $CODEBASE/weeklyreport.py $GREP #> /tmp/whathappened_weeklyreport.log 2>&1
+$PYTHON $CODEBASE/weeklyreport.py $GREP > /tmp/whathappened_weeklyreport.log 2>&1
